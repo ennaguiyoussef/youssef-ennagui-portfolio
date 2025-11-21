63c04ac876f60f06c0151de478f0f8d605540bf4
Youssef Ennagui <youssef.ennagui@usmba.ac.ma>
2025-11-21 02:32:23 +0100

    modifier les informations <<les liens de social media>>

--- src/app/pages/sections/contacts/contacts.ts
+++ src/app/pages/sections/contacts/contacts.ts
@@ -9,8 +9,8 @@ import { Component } from '@angular/core';
 export class ContactsSection {
   contacts = [
     { icon: 'bi bi-envelope-fill', label: 'Email', value: 'youssef.ennagui@usmba.ac.ma', link: 'mailto:youssef.ennagui@gmail.com' },
-    { icon: 'bi bi-linkedin', label: 'LinkedIn', value: 'Youssef Ennagui', link: 'https://linkedin.com/in/youssef-ennagui' },
-    { icon: 'bi bi-github', label: 'GitHub', value: 'youssef-ennagui', link: 'https://github.com/youssef-ennagui' }
+    { icon: 'bi bi-linkedin', label: 'LinkedIn', value: 'Youssef Ennagui', link: 'https://www.linkedin.com/in/youssef-ennagui-b1862b37a/' },
+    { icon: 'bi bi-github', label: 'GitHub', value: 'youssef-ennagui', link: 'https://github.com/ennaguiyoussef' }
   ];
 }
 
